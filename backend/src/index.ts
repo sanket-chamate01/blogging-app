2aa1eeee098d087b2d58624c4d8f559a8300e3d8
sanket-chamate01 <sanket.chamate2001@gmail.com>
2024-12-24 19:46:38 +0530

    signup done

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,6 +2,10 @@ import { Hono } from 'hono'
 import user from './routes/user'
 import blog from './routes/blog'
 
+import { PrismaClient } from '@prisma/client/edge'
+import { withAccelerate } from '@prisma/extension-accelerate'
+
+
 const app = new Hono().basePath('/api/v1')
 
 app.route('/user', user)
--- backend/src/routes/user.ts
+++ backend/src/routes/user.ts
@@ -1,8 +1,28 @@
 import { Hono } from 'hono'
+import { PrismaClient } from '@prisma/client/edge'
+import { withAccelerate } from '@prisma/extension-accelerate'
 
-const user = new Hono().basePath('user')
+const user = new Hono<{
+    Bindings: {
+        DATABASE_URL: string
+    }
+}>().basePath('user')
 
 user.post('signup', async (c) => {
+    const prisma = new PrismaClient({
+        datasourceUrl: c.env.DATABASE_URL,
+    }).$extends(withAccelerate())
+
+    const body = await c.req.json()
+
+    await prisma.user.create({
+        data: {
+            email: body.email,
+            password: body.password,
+            name: body.name
+        }
+    })
+
     return c.text('user signup')
 })
 
