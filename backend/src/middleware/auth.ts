e9c5794be257824271278c99166c59fabdbce2f8
sanket-chamate01 <sanket.chamate2001@gmail.com>
2024-12-26 16:35:58 +0530

    modified auth middleware

--- backend/src/middleware/auth.ts
+++ backend/src/middleware/auth.ts
@@ -2,23 +2,31 @@ import { MiddlewareHandler } from "hono";
 import { verify } from "hono/jwt";
 
 export const authMiddleware: MiddlewareHandler = async (c, next) => { 
-    const header = c.req.header('Authorization') || ''
-    const token = header.split(' ')[1]
-    if(!token) {
-        c.status(401)
+    try {
+        const header = c.req.header('Authorization') || ''
+        const token = header.split(' ')[1]
+        if(!token) {
+            c.status(401)
+            return c.json({
+                message: 'Unauthorized'
+            })
+        }
+        const decoded = await verify(token, c.env.JWT_SECRET)
+        if(decoded) {
+            c.set('userId', decoded.id);
+            console.log('decoded: ', decoded)
+            return await next()
+        } else {
+            c.status(401)
+            return c.json({
+                message: 'Unauthorized'
+            })
+        }
+    } catch (error) {
+        console.error("Error verifying token: ", error)
+        c.status(500)
         return c.json({
-            message: 'Unauthorized'
-        })
-    }
-    const decoded = await verify(token, c.env.JWT_SECRET)
-    if(decoded) {
-        c.set('userId', decoded.id);
-        console.log('decoded: ', decoded)
-        return await next()
-    } else {
-        c.status(401)
-        return c.json({
-            message: 'Unauthorized'
+            error: 'Internal server error'
         })
     }
 }
